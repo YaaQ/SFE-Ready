248ed93d9c0406c388a3c6b95a1aa833279697ab
Svetlana Bykovskaya <bykovskaya.svetlana@gmail.com>
2015-11-07 15:16:47 +0300

    Update SFEexerput.R

--- QID-3147-SFEexerput/SFEexerput.R
+++ QID-3147-SFEexerput/SFEexerput.R
@@ -28,20 +28,20 @@ k = 0.001  	# initial exercise price
 t = 100  	# initial difference between two ks
 while (t >= 1e-05) {
     # acceptable value for difference
-    y = (log(s0/k) + (b - 1/2 * (sig^2)) * tau)/(sig * sqrt(tau))  # y for BS 
-    yk = -1/(sig * sqrt(tau) * k)  # FOC of y respect to k
-    cdfnyk1 = -exp(-1 * y^2/2) * (yk/sqrt(2 * pi))  # FOC of PI(-y) respect to k
+    y = (log(s0/k) + (b - 1/2 * (sig^2)) * tau)/(sig * sqrt(tau))       # y for BS 
+    yk = -1/(sig * sqrt(tau) * k)                                       # FOC of y respect to k
+    cdfnyk1 = -exp(-1 * y^2/2) * (yk/sqrt(2 * pi))                      # FOC of PI(-y) respect to k
     cdfnyk2 = -exp(-1 * (y + sig * sqrt(tau))^2/2) * (yk/sqrt(2 * pi))  # FOC of PI(-y-sig*sqrt(tau))
     
     pk = exp(-r * tau) * k * pnorm(-1 * y) - exp((b - r) * tau) * s0 * pnorm(-1 * 
         y - sig * sqrt(tau))  # BC's put option price 
     pkk = exp(-r * tau) * pnorm(-1 * y) + exp(-1 * r * tau) * k * cdfnyk1 - 
-        exp((b - r) * tau) * s0 * cdfnyk2  # FOC of put price respect to k
-    fk = exp(-d * tau) * s0 + pk - V/F * k  # equation 2-23 see page 30
-    fkk = pkk - V/F  #FOC of equation respect to k
-    k0 = k  # old k
-    k = k - fk/fkk  # new k
-    t = k - k0  # difference 
+        exp((b - r) * tau) * s0 * cdfnyk2         # FOC of put price respect to k
+    fk = exp(-d * tau) * s0 + pk - V/F * k        # equation 2-23 see page 30
+    fkk = pkk - V/F                               # FOC of equation respect to k
+    k0 = k                                        # old k
+    k = k - fk/fkk                                # new k
+    t = k - k0                                    # difference 
 }
 
 # output
