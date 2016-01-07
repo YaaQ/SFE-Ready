2356224f637d4aec4089959e1cc1f0d32b6c5207
polarstern <bykovskaya.svetlana@gmail.com>
2016-01-07 18:36:37 +0300

    update

--- QID-1955-SFElshill/SFElshill.R
+++ QID-1955-SFElshill/SFElshill.R
@@ -10,43 +10,35 @@ lapply(libraries, function(x) if (!(x %in% installed.packages())) {
 })
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
-# load data
-DS  = read.table("FSE_LSE.dat")
-D   = DS[, 1]                          # date
-S   = DS[, 2:43]                       # S(t)
-s   = log(S)                           # log(S(t))
-end = length(D)
-r   = s[2:end, ] - s[1:(end - 1), ]    # r(t)
-n   = length(r)                        # sample size
-t   = c(1:n)                           # time index, t
+# load data for FSE and LSE
+DS = read.csv("FSE_LSE_2014.dat")
+D  = DS[, 1]                           # date
+S  = DS[, 2:43]                        # S(t)
+r  = diff(as.matrix(log(S)))           # r(t) = log(S(t)) - log(S(t-1)) 
+n  = length(r)                         # sample size
+d  = NCOL(r)                           # columns or r
 
 # Right tail index regression and Hill estimator
 m1 = 10  # m1 largest observations
 m2 = 25  # m2 largest observations
 
-rsorted   = apply(r, 2, sort)
-rswitched = flipud(rsorted)
+rsorted = apply(r, 2, sort, decreasing = TRUE)
 
-xs1 = log(rswitched)
-x1  = xs1[1:m1, ]
-ys1 = log(c(1:m1)/n)
-y1  = kronecker(matrix(1, 1, 42), ys1)
+x1 = log(rsorted[1:m1, ])
+y1 = matrix(rep(log(c(1:m1)/n), d), ncol = d)
 
-xs2 = log(rswitched)
-x2  = xs2[1:m2, ]
-ys2 = log(c(1:m2)/n)
-y2  = kronecker(matrix(1, 1, 42), ys2)
+x2 = log(rsorted[1:m2, ]) 
+y2 = matrix(rep(log(c(1:m2)/n), d), ncol = d)
 
-ls1   = matrix(0, 42, 2)
-ls2   = matrix(0, 42, 2)
-hill1 = matrix(0, 42, 1)
-hill2 = matrix(0, 42, 1)
+ls1 = ls2 = hill1 = hill2 = numeric(d)
 
-for (i in 1:42) {
-    ls1[i, ]   = polyfit(x1[, i], y1[, i], 1)
-    ls2[i, ]   = polyfit(x2[, i], y2[, i], 1)
-    hill1[i, ] = 1/(mean(x1[1:(m1 - 1), i]) - x1[m1, i])
-    hill2[i, ] = 1/(mean(x2[1:(m2 - 1), i]) - x2[m2, i])
+for (i in 1:d){
+  ls1[i]   = lm(y1[, i] ~ x1[, i])$coef[2]
+  ls2[i]   = lm(y2[, i] ~ x2[, i])$coef[2]
+  hill1[i] = 1/(mean(x1[1:(m1-1), i]) - x1[m1, i])
+  hill2[i] = 1/(mean(x2[1:(m2-1), i]) - x2[m2, i])
 }
 
-(Y = cbind(-ls1[, 1], -ls2[, 1], hill1, hill2))
+Y = cbind(-ls1, -ls2, hill1, hill2)
+colnames(Y) = c("LS_m1", "LS_m2", "Hill_m1", "Hill_m2")
+round(Y, 2)
